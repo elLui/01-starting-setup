523bf95ada0fded09bb3d13c17239280c2a70805
elluis.codes <elluis.codes@gmail.com>
2021-05-18 11:27:49 -0500

    added started building user input form for expenses added css code for expenses

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+
+    return
+    <form>
+        <div className='new-expense__controls'>
+            <div className='new-expense__control'>
+                <label>Title</label>
+                <input type='number' min="0.01" step="0.01" />
+            </div>
+            <div className='new-expense__control'>
+                <label>Title</label>
+                <input type='number' min="0.01" step="0.01" />
+            </div><div className='new-expense__control'>
+                <label>Title</label>
+                <input type='number' min="0.01" step="0.01" />
+            </div>
+        </div>
+
+    </form>
+};
+
+
+export default ExpenseForm;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './NewExpenses.css'
+
+const NewExpense = () => {
+
+
+
+
+    return
+    <div className="new-expenses">
+
+
+
+
+    </div>
+
+
+};
+
+
+export default NewExpense;
